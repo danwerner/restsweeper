29b07f4a466aec8397dbeeb0c64f9e095bf3d08d
Daniel Werner <daniel.d.werner@googlemail.com>
2011-02-13 15:46:07 +0100

    Removed Compojure dependencies completely

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
                  [net.cgrand/moustache "1.0.0-SNAPSHOT"]
                  [enlive "1.0.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.3.5"]
-                 [ring/ring-core "0.3.5"]
-                 [compojure "0.3.2"]]
+                 [ring/ring-core "0.3.5"]]
   :dev-dependencies [[vimclojure/server "2.2.0"]])
--- src/restsweeper/templates.clj
+++ src/restsweeper/templates.clj
@@ -1,8 +1,6 @@
 (ns restsweeper.templates
   (:use [restsweeper game hash]
         [clojure.contrib.seq :only [indexed]]
-        [compojure.html :only [doctype html include-css include-js link-to
-                               xhtml-tag]]
         [net.cgrand.enlive-html]
         [ring.util.response :only [response redirect]])
   (:require [clojure.string :as str]))
@@ -18,17 +16,6 @@
    ;["Hard"   30 16 99]
    ])
 
-(defn html-base
-  [headers & body]
-  (html
-    (doctype :xhtml-strict)
-    (xhtml-tag "en"
-      [:html
-        [:head
-          [:title "RESTsweeper"]
-           (apply concat headers)]
-        "\n"
-        [:body nil body]])))
 
 (defn cell-format [cell game-over?]
   "Returns a vector [css-class content] to visually represent a cell."
@@ -81,43 +68,6 @@
     (when *debug*
       (html-content "[" (str/join "<br>" (map (comp str vec) board)) "]")))
 
-(defn board-page* [h w board]
-  (let [game-lost  (game-lost? board)
-        game-won   (game-won? board)
-        game-over  (or game-lost game-won)
-        message    (cond game-lost "You lose :-("
-                         game-won  "You win :-)")]
-    (html-base
-      [(include-css "/static/css/game.css")
-       (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"
-                   "/static/js/jquery.rightClick.js"
-                   "/static/js/game.js")]
-      [:table#board
-        (for [[y row] (indexed board)]
-          [:tr
-            (for [[x cell] (indexed row)]
-              (let [[class content] (cell-format cell game-over)
-                    div             [:div {:class class} content]]
-                [:td.cell
-                  (if-not (or game-over (uncovered? cell))
-                    (let [uncover-url     (if-not (flag? cell)
-                                            (action-url uncover y x h w board))
-                          flag-url        (action-url flag y x h w board)]
-                      ; Left click -> href, right click -> rel
-                      [:a {:href uncover-url :rel flag-url}
-                        div])
-                    div)]))])
-          [:tr [:td#message {:colspan w} (or message "&nbsp;")]]]
-
-      [:p (link-to "/" "&laquo; Menu")]
-
-      (when *debug*
-        [:p "["
-          (interpose [:br]
-            (for [row board]
-              (str (vec row))))
-        "]" ])
-      )))
 
 (deftemplate page "templates/base.html" [body styles scripts]
   [[:link (attr= :rel "stylesheet")]]
@@ -127,6 +77,7 @@
                       (set-attr :src script))
   [:body]           (content body))
 
+
 (defsnippet main-page "templates/main.html" [:body :> any-node] []
   [:ul#newgame :li]
   (clone-for [[name w h m] difficulty]
@@ -134,19 +85,3 @@
            (set-attr :href (format "/new/%dx%dx%d" w h m))
            (content (format "%s - %dx%d with %d mines"
                             name w h m)))))
-
-(defn main-page* []
-  (html-base
-    nil
-    [:h1 "RESTsweeper"]
-    [:h2 "New game"]
-    [:ul
-      (for [[name w h m] difficulty]
-        [:li (link-to (format "/new/%dx%dx%d" w h m)
-                      (format "%s - %dx%d with %d mines" name w h m))])]
-    [:h2 "Resources"]
-    [:ul
-      [:li (link-to "http://en.wikipedia.org/wiki/Minesweeper_(video_game)" "Read Manual")]
-      [:li (link-to "http://github.com/danwerner/restsweeper" "Download Source")]]))
-
-
